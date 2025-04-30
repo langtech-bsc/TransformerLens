e35fac12a65e02d957b1e90d30fa2c85ca06778d
Júlia Falcão <juliafsfalcao@hotmail.com>
2025-04-30 14:14:58 +0200

    hardcode get_official_model_name to be passthrough and just return the input

--- transformer_lens/loading_from_pretrained.py
+++ transformer_lens/loading_from_pretrained.py
@@ -729,14 +729,7 @@ def get_official_model_name(model_name: str):
     """
     Returns the official model name for a given model name (or alias).
     """
-    model_alias_map = make_model_alias_map()
-    official_model_name = model_alias_map.get(model_name.lower(), None)
-    if official_model_name is None:
-        raise ValueError(
-            f"{model_name} not found. Valid official model names (excl aliases): {OFFICIAL_MODEL_NAMES}"
-        )
-    return official_model_name
-
+    return model_name
 
 def convert_hf_model_config(model_name: str, **kwargs):
     """
